6e00c09fc72e5845740cef56c29023bd54dde767
aanguera <aanguera.65@gmail.com>
2016-09-15 22:03:21 +0200

    This is the front page of the shop

---
+++ PetShopFront.php
@@ -0,0 +1,34 @@
+<?php
+	/* Program: PetShopFront.php
+	 * Des:		Displays opening page for Pet Shop
+	 */
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb" dir="ltr">
+<head>
+	 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<html lang="en-GB">
+<meta charset="UTF-8"> 
+
+<body style="margin: 0">
+
+<table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td style="text-align: center" valign="top">
+<img src="../images/awning-top.gif" alt="awning" />
+</td>
+</tr>
+<tr>
+<td style="text-align: center" valign="top">
+<img src="../images/Name.gif" alt="Pet Store">
+<p style="margin-top: 40pt">
+<img src="../images/lizard-front.jpg" alt="animal picture" height="186" width="280" />
+<h2>Looking for a new friend?</h2>
+<p>Check out our
+<a href="PetCatalog.php">Pet Catalog.</a>
+<br> We may have just what you’re looking for.</p>
+</td>
+</tr>
+</table>
+
+</body>
+</html>	
